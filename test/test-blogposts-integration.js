68796a1fbd9ba3204dd737dd8a38ebd4d1647a52
Evan Thompson <evanbrowningthompson@gmail.com>
2018-01-14 21:43:30 -0500

    added hooks and skeleton for endpoint tests

--- test/test-blogposts-integration.js
+++ test/test-blogposts-integration.js
@@ -13,3 +13,64 @@ const { TEST_DATABASE_URL } = require('../config');
 
 chai.use(chaiHttp);
 
+function seedBlogpostData() {
+  console.info('seeding blogpost data');
+  const seedData = [];
+
+  for(i = 1; i <= 10; i++) {
+    seedData.push(generateBlogpostData());
+  }
+
+  return Blogpost.insertMany(seedData);
+}
+
+function generateBlogpostData() {
+  return {
+    title: `${ faker.random.word() } ${ faker.random.word() }`,
+    author: {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName()
+    },
+    content: faker.lorem.paragraph();
+  };
+}
+
+function tearDownDb() {
+  console.warn('tearing down database');
+  mongoose.connection.dropDatabase();
+}
+
+describe('Blogposts API resource', function() {
+
+  before(function() {
+    return runserver(TEST_DATABASE_URL);
+  });
+
+  beforeEach(function() {
+    return seedBlogpostData();
+  });
+
+  afterEach(function() {
+    return tearDownDb();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  describe('GET endpoint', function() {
+
+  });
+
+  describe('POST endpoint', function() {
+
+  });
+
+  describe('PUT endpoint', function() {
+
+  });
+
+  describe('DELETE endpoint', function() {
+
+  });
+});
